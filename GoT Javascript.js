aa8ced7d5e555005d77ca1fa1e42961106117a0c
Sam Stoppani <samstoppani@gmail.com>
2020-01-11 21:16:02 +0000

    GoT

---
+++ GoT Javascript.js
@@ -0,0 +1,17 @@
+var baratheonAudio = new Audio();
+baratheonAudio.src = "audio/baratheon-audio.wav";
+
+var greyjoyAudio = new Audio();
+greyjoyAudio.src = "audio/greyjoy-audio.wav";
+
+var lannisterAudio = new Audio();
+lannisterAudio.src = "audio/lannister-audio.wav";
+
+var martellAudio = new Audio();
+martellAudio.src = "audio/martell-audio.mp3";
+
+var starkAudio = new Audio();
+starkAudio.src = "audio/stark-audio.wav";
+
+var tullyAudio = new Audio();
+tullyAudio.src = "audio/tully-audio.mp3";
